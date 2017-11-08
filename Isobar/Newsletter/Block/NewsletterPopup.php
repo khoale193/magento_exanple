61768a46efb6da477cb5b04ba26cb9917e3886f3
Le Anh Khoa <Khoa.Le@isobar.com>
2017-11-08 16:55:09 +0700

    Coding requirement added

--- Isobar/Newsletter/Block/NewsletterPopup.php
+++ Isobar/Newsletter/Block/NewsletterPopup.php
@@ -2,7 +2,58 @@
 namespace Isobar\Newsletter\Block;
 
 use Magento\Newsletter\Block\Subscribe;
+use Magento\Newsletter\Model\SubscriberFactory;
+use Magento\Framework\View\Element\Template\Context;
+use Magento\Customer\Model\Session;
 
 class NewsletterPopup extends Subscribe
 {
+    /**
+     * @var Session
+     */
+    protected $session;
+
+    /**
+     * Subscriber factory
+     *
+     * @var SubscriberFactory
+     */
+    protected $subscriberFactory;
+
+    /**
+     * NewsletterPopup constructor.
+     *
+     * @param Context $context
+     * @param Session $customerSession
+     */
+    public function __construct(
+        Context $context,
+        Session $customerSession,
+        SubscriberFactory $subscriberFactory
+    )
+    {
+        $this->session = $customerSession;
+        $this->subscriberFactory = $subscriberFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * Check current user have subscribed
+     *
+     * @return bool
+     */
+    public function isShowPopup()
+    {
+        if ($this->session->isLoggedIn()) {
+            $customerId = $this->session->getCustomerId();
+            // Check logged in customer have subscribed
+            $customerSubscriber = $this->subscriberFactory->create()->loadByCustomerId($customerId);
+
+            if ($customerSubscriber->getId()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
